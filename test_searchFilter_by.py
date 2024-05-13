f7333ecadcafa14b5a8ade3297e9c0189f8aa429
0lena <helena.grebenschikova@gmail.com>
2024-05-13 14:25:04 -0400

    Initial

---
+++ test_searchFilter_by.py
@@ -0,0 +1,12 @@
+
+from selenium import webdriver
+import pytest
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver = webdriver.Chrome()
+
+
+def test_filter_by_stops():
+    pass
