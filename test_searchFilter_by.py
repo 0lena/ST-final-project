d614adc357d71faef6a6416cd988caa98062ba30
0lena <helena.grebenschikova@gmail.com>
2024-05-13 17:19:34 -0400

    First modification

--- test_searchFilter_by.py
+++ test_searchFilter_by.py
@@ -3,10 +3,8 @@ from selenium import webdriver
 import pytest
 import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
 
 driver = webdriver.Chrome()
-
 
 def test_filter_by_stops():
     driver.get(
